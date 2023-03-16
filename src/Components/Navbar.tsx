49e806855680c2659353683693d7f5c1d18bbb5b
Abhishek-90 <holaniabhishek90@gmail.com>
2023-03-17 00:15:13 +0530

    completed navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,22 +2,24 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="w-full fixed max-w-[1580px] p-[12px] flex justify-between z-10 text-white">
-      <Link to="/">
-        <div className="coindum-logo text-2xl font-semibold border-solid border-2 border-white tracking-wider p-1 rounded">
-          <p>COINDUM</p>
+    <div className="flex w-full fixed justify-center">
+      <nav className="w-full max-w-1580px p-[12px] flex justify-between z-50 text-white">
+        <Link to="/">
+          <div className="coindum-logo text-2xl font-semibold border-solid border-2 border-white tracking-wider p-1 rounded">
+            <p>COINDUM</p>
+          </div>
+        </Link>
+        <div className="links p-2">
+          <ul className="list-none flex justify-between text-lg font-semibold">
+            <li className="mx-10">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="#">Market</Link>
+            </li>
+          </ul>
         </div>
-      </Link>
-      <div className="links p-2">
-        <ul className="list-none flex justify-between text-lg font-semibold">
-          <li className="mx-10">
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="#">Market</Link>
-          </li>
-        </ul>
-      </div>
+      </nav>
     </div>
   );
 }
