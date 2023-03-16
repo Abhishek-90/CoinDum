70fd52fdfeebe8cc49edb8e39a4f8a0d67f6d7d3
Abhishek-90 <holaniabhishek90@gmail.com>
2023-03-17 00:15:04 +0530

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import CoinDetails from "./Pages/CoinDetails";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-gradient-to-t from-[#020024] via-[#02062a] to-[#0b1260] ">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coin/detail/*" element={<CoinDetails />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
