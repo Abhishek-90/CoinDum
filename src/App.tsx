794c059248c60c2be938b2dc01b1c77a495229b2
Abhishek-90 <holaniabhishek90@gmail.com>
2023-04-05 16:02:53 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/coinDetails" element={<CoinDetails />} />
+          <Route path="/coinDetails/*" element={<CoinDetails />} />
         </Routes>
       </Router>
     </>
