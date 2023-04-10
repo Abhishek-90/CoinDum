eea7e4838a682903be451fb39a5c80a8dfca3a4e
Abhishek-90 <holaniabhishek90@gmail.com>
2023-04-10 15:03:08 +0530

    Added loader

--- src/Components/CoinList.tsx
+++ src/Components/CoinList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import fetchData, { coinListOptions } from "../utils/fetchData";
+import Loader from "./Loader";
 
 const RESULTS_PER_PAGE = 30;
 
@@ -9,6 +10,8 @@ function CoinList() {
   const [top4CoinData, setTop4CoinData] = useState([]);
   const [page, setPage] = useState<number>(1);
   const [pagination, setPagination] = useState<number[]>([]);
+  const [isFetchingCoins, setIsFetchingCoins] = useState<boolean>(true);
+  const [isFetching4Coins, setIsFetching4Coins] = useState<boolean>(true);
   const totalPages = useRef(10);
 
   useEffect(() => {
@@ -25,6 +28,7 @@ function CoinList() {
         coinListOptions
       );
       setCoinData(response.coins);
+      setTimeout(() => setIsFetchingCoins(false), 1000);
     };
     runFetchData();
   }, [page]);
@@ -70,6 +74,7 @@ function CoinList() {
       setTop4CoinData(response.coins);
     };
     runFetchData();
+    setTimeout(() => setIsFetching4Coins(false), 1000);
   }, []);
 
   function numberWithCommas(x: string) {
@@ -78,68 +83,80 @@ function CoinList() {
 
   return (
     <div className="container">
-      <div className="top-4-coins-wrapper">
-        <div className="top-4-coins">
-          {top4CoinData.length > 0 &&
-            top4CoinData.map((coin: any, index: number) => (
-              <div className="coin">
+      {isFetching4Coins && <Loader />}
+      {!isFetching4Coins && (
+        <div className="top-4-coins-wrapper">
+          <div className="top-4-coins">
+            {top4CoinData.length > 0 &&
+              top4CoinData.map((coin: any, index: number) => (
+                <div className="coin">
+                  <Link
+                    className="coin-link"
+                    to={`/coinDetails?uuid=${coin.uuid}`}
+                    key={index}
+                  >
+                    <img
+                      className="coin-img"
+                      src={coin.iconUrl}
+                      alt="coin-img"
+                    />
+                    <p className="coin-name">{coin.name}</p>
+                    <p className="coin-price">
+                      $ {parseFloat(coin.price).toFixed(2)}
+                    </p>
+                    <p
+                      className={`${
+                        parseFloat(coin.change) >= 0 ? "green" : "red"
+                      }`}
+                    >
+                      {coin.change} %
+                    </p>
+                  </Link>
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+
+      <h2 className="h2">Market Update</h2>
+
+      {/* All Coin Details */}
+      {isFetchingCoins && <Loader />}
+      {!isFetchingCoins && (
+        <div className="coin-detail-wrapper">
+          <div className="coin-detail">
+            <div className="header">
+              <p>Coin</p>
+              <p>Price</p>
+              <p>24H Change</p>
+              <p>Market Cap</p>
+            </div>
+
+            {coinData.length > 0 &&
+              coinData.map((coin: any) => (
                 <Link
-                  className="coin-link"
+                  className="detail-row"
                   to={`/coinDetails?uuid=${coin.uuid}`}
-                  key={index}
+                  key={coin.uuid}
                 >
-                  <img className="coin-img" src={coin.iconUrl} alt="coin-img" />
-                  <p className="coin-name">{coin.name}</p>
-                  <p className="coin-price">
-                    $ {parseFloat(coin.price).toFixed(2)}
-                  </p>
+                  <span>
+                    <img src={coin.iconUrl} alt={coin.name} />
+                    <p>{coin.symbol}</p>
+                  </span>
+                  <p>{"$ " + parseFloat(coin.price).toFixed(2)}</p>
                   <p
                     className={`${
                       parseFloat(coin.change) >= 0 ? "green" : "red"
                     }`}
                   >
-                    {coin.change} %
+                    {coin.change + " %"}
                   </p>
+                  <p>{numberWithCommas(coin.marketCap)}</p>
                 </Link>
-              </div>
-            ))}
-        </div>
-      </div>
-
-      {/* All Coin Details */}
-      <div className="coin-detail-wrapper">
-        <div className="coin-detail">
-          <div className="header">
-            <p>Coin</p>
-            <p>Price</p>
-            <p>24H Change</p>
-            <p>Market Cap</p>
+              ))}
           </div>
-
-          {coinData.length > 0 &&
-            coinData.map((coin: any) => (
-              <Link
-                className="detail-row"
-                to={`/coinDetails?uuid=${coin.uuid}`}
-                key={coin.uuid}
-              >
-                <span>
-                  <img src={coin.iconUrl} alt={coin.name} />
-                  <p>{coin.symbol}</p>
-                </span>
-                <p>{"$ " + parseFloat(coin.price).toFixed(2)}</p>
-                <p
-                  className={`${
-                    parseFloat(coin.change) >= 0 ? "green" : "red"
-                  }`}
-                >
-                  {coin.change + " %"}
-                </p>
-                <p>{numberWithCommas(coin.marketCap)}</p>
-              </Link>
-            ))}
         </div>
-      </div>
+      )}
 
       {/* Pagination */}
       <div className="pagination">
