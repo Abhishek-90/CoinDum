c1bba3d933981f431c62583484ff485978fa4b8e
Abhishek-90 <holaniabhishek90@gmail.com>
2023-04-11 12:56:31 +0530

    Animation added to hero compoennt

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,10 +1,14 @@
+import { bitcoinImg, etherium } from "../assets/Links";
+
 function Hero() {
   return (
     <div className="container">
+      <img className="float-btc" src={bitcoinImg} alt="btc-float" />
       <div className="intro">
         <h2>Trade and Track</h2>
         <p>Crypto Currencies</p>
       </div>
+      <img className="float-eth" src={etherium} alt="eth-float" />
     </div>
   );
 }
