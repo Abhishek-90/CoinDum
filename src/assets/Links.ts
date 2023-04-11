56e03c924eadd51c584215e38431db72eed87013
Abhishek-90 <holaniabhishek90@gmail.com>
2023-04-11 12:35:59 +0530

    Shared img resource

---
+++ src/assets/Links.ts
@@ -0,0 +1,4 @@
+export const bitcoinImg: string =
+  "https://user-images.githubusercontent.com/105128267/220607693-29987a1c-8f0d-4835-9bfe-3b5c9f326b17.png";
+export const etherium: string =
+  "https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png";
